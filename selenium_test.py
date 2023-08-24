0afe9f47f392c603d46b9de50b7b559d0b8d88d7
Leziga Bakor <85461869+leziga-bakor-epam@users.noreply.github.com>
2023-08-24 11:02:20 +0000

    complete first test

--- selenium_test.py
+++ selenium_test.py
@@ -1,1 +1,15 @@
 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver.get("https://www.python.org")
+print(driver.title)
+search_bar = driver.find_element_by_name("q")
+search_bar.clear()
+search_bar.send_keys("getting started with python")
+search_bar.send_keys(Keys.RETURN)
+print(driver.current_url)
+driver.close()
